337a9648d8a6e9ead7f423958741e3fff50bbce0
root <root@ip-10-42-22-172.ec2.internal>
2025-10-15 16:56:55 +0000

    fix

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,6 +9,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	if(n>0){
+		printf("is positive/n")
+	}
+			else if(n<0){
+				printf("is negative/n")
+			}
+					else{
+						printf("is zero/n")
+					}
+
 	return (0);
 }
