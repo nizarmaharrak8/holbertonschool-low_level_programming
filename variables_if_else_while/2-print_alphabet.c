1c865ca979f70a1a947e46a5597cfa82d9a6f063
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-10-16 16:16:31 +0000

    fix

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Generates a random number and prints whether
+ * the last digit is >5, is 0, or <6 and not 0.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char x;
+for(x=a;x<=z;x++)
+putchar(x)
+}
+
+return (0);
+}
