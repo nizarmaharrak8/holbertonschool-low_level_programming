6664a7578de22210c45c254f2eba7c4e3062e4c2
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-10-18 16:54:59 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+/**
+* _putchar - writes a character to stdout
+* @c: The character to print
+*
+* Return: On success 1, on error -1
+*/
+int _putchar(char c); // function prototype
 
 int main(void)
 {
-printf("_putchar\n");
+char *str = "_putchar\n";
+int i = 0;
+
+while (str[i])
+{
+_putchar(str[i]);
+i++;
+}
+
 return 0;
 }
+
