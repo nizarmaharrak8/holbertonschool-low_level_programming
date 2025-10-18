5f9c00445d1f6b000f6ffaccd10757a8657e008a
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-10-18 18:21:32 +0000

    fix

--- functions_nested_loops/10-main.c
+++ functions_nested_loops/10-main.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-int n;
+ int n;
 
 n = add(89, 9);
 printf("%d\n", n);
