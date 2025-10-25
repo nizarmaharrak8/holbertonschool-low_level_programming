11d8bd513a69f63609c247dc106a09b9bb07d028
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-10-25 20:20:04 +0000

    fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,28 +8,23 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, num_started = 0;
 	unsigned int result = 0;
+	int i = 0, sign = 1;
 
-	if (!s)
-		return (0);
-
-	while (s[i] != '\0')
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
 			sign *= -1;
-		else if (s[i] == '+')
-			;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			num_started = 1;
-			result = result * 10 + (s[i] - '0');
-		}
-		else if (num_started)
-			break;
 		i++;
 	}
 
-	return (sign * (int)result);
-}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = (result * 10) + (s[i] - '0');
+		i++;
+	}
 
+	if (sign == -1)
+		return (-((int)result));
+	return ((int)result);
+}
