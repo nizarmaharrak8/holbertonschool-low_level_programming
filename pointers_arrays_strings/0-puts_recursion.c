55b0e8315afb79eb227e1870f6889e16f5c3d9e5
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-10-29 22:08:36 +0000

    0-puts_recursion.c

---
+++ pointers_arrays_strings/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to print
+ */
+void _puts_recursion(char *s)
+{
+    if (*s == '\0') /* base case: end of string */
+    {
+        _putchar('\n');
+        return;
+    }
+
+    _putchar(*s); /* print current character */
+    _puts_recursion(s + 1); /* recursive call on the rest of the string */
+}
+
