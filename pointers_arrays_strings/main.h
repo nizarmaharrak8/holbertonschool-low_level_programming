fcb3da1ea41e22aed04379a6d817ba01da502991
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-10-29 20:23:24 +0000

    0-memset.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
 void print_rev(char *s);
 void rev_string(char *s);
 int _putchar(char c);
