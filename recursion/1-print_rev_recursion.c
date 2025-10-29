2de4eb7d6bc3e2de1edbb170ac222519da927b3d
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-10-29 23:24:47 +0000

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -3,13 +3,15 @@
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: string to print
+ *
+ * Return: void
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0') /* نهاية السلسلة */
-        return;
-
-    _print_rev_recursion(s + 1); /* استدعاء الدالة على الحرف التالي */
-    _putchar(*s); /* طباعة الحرف بعد الرجوع من الاستدعاء */
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
 
