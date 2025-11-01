c98ab1c948a099e855e78d3e0bf2bfa1c79ce25b
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-11-01 22:25:36 +0000

    c3

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,8 +1,28 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
 
-int main(int argc, char *argv[]);
+/**
+ * main - Multiplies two numbers.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of strings containing the arguments.
+ *
+ * Return: 0 if successful, 1 if the program does not receive two arguments.
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
 
-#endif
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
+}
 
