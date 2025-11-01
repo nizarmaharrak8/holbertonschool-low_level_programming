5c51613e8caa8772ef153f4df70bdce1cc8b4842
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-11-01 22:49:14 +0000

    fix file 3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,20 +8,20 @@
  *
  * Return: 0 if successful, 1 if the program does not receive two arguments.
  */
-int main(int argc, char *argv[])
+	int main(int argc, char *argv[])
 {
-int num1, num2, result;
+	int num1, num2, result;
 
-if (argc != 3)
+	if (argc != 3)
 {
-printf("Error\n");
-return (1);
+	printf("Error\n");
+	return (1);
 }
 
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-result = num1 * num2;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
 
-printf("%d\n", result);
-return (0);
+	printf("%d\n", result);
+	return (0);
 }
