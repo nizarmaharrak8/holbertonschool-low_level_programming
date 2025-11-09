a2cb57963a292bee33096542bf1b32f4fce4bf63
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-11-09 09:07:57 +0000

    dog.h

---
+++ more_malloc_free/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - represents a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: This structure holds basic information
+ * about a dog including its name, age, and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
