0ebacb4a02ad55dcb608ed5073325763d48519c1
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-11-09 10:10:57 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,38 +4,52 @@
 #include "dog.h"
 
 /**
- * new_dog - creates a new dog
+ * new_dog - creates a new dog_t instance
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Return: pointer to the new dog (dog_t), or NULL on failure
+ * Return: pointer to the new dog_t, or NULL on failure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *d;
-    char *name_copy, *owner_copy;
+	dog_t *d;
+	char *name_copy, *owner_copy;
 
-    d = malloc(sizeof(dog_t));
-    if (d == NULL)
-        return (NULL);
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
 
-    name_copy = (name != NULL) ? strdup(name) : NULL;
-    owner_copy = (owner != NULL) ? strdup(owner) : NULL;
+	name_copy = NULL;
+	owner_copy = NULL;
 
-    if ((name != NULL && name_copy == NULL) ||
-        (owner != NULL && owner_copy == NULL))
-    {
-        free(name_copy);
-        free(owner_copy);
-        free(d);
-        return (NULL);
-    }
+	if (name != NULL)
+	{
+		name_copy = malloc(strlen(name) + 1);
+		if (name_copy == NULL)
+		{
+			free(d);
+			return (NULL);
+		}
+		strcpy(name_copy, name);
+	}
 
-    d->name = name_copy;
-    d->age = age;
-    d->owner = owner_copy;
+	if (owner != NULL)
+	{
+		owner_copy = malloc(strlen(owner) + 1);
+		if (owner_copy == NULL)
+		{
+			free(name_copy);
+			free(d);
+			return (NULL);
+		}
+		strcpy(owner_copy, owner);
+	}
 
-    return (d);
+	d->name = name_copy;
+	d->age = age;
+	d->owner = owner_copy;
+
+	return (d);
 }
 
