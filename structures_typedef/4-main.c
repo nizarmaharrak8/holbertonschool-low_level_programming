22afb16cb67849a5057f8d0c22172912c6bb990e
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-11-09 10:23:27 +0000

    5-free_dog.c

--- structures_typedef/4-main.c
+++ structures_typedef/4-main.c
@@ -1,28 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
 
 /**
- * main - check the code for new_dog
+ * main - check the code
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
 	dog_t *my_dog;
 
 	my_dog = new_dog("Poppy", 3.5, "Bob");
-	if (my_dog == NULL)
-		return (1);
-
 	printf("My name is %s, and I am %.1f :) - Woof!\n",
 	       my_dog->name, my_dog->age);
 
-	/* Free allocated memory to avoid memory leaks */
-	free(my_dog->name);
-	free(my_dog->owner);
-	free(my_dog);
-
+	free_dog(my_dog);
 	return (0);
 }
 
