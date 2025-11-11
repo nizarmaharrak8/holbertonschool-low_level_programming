c9044c7afd1b5912f44cb32a2ce7b97608a7e1e1
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-11-11 10:50:19 +0000

    add

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - creates a 2D array of integers
+ * @width: number of columns
+ * @height: number of rows
+ *
+ * Return: pointer to 2D array, or NULL on failure
+ */
+void print_name(char *name, void (*f)(char *))
+{
+
+}
+
