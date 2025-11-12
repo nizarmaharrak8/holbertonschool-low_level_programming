bcd78e004d84d15131c060c4918755b00b3e9a75
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-11-12 11:26:50 +0000

    add

---
+++ function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int (*operation)(int, int);
+
+	
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	
+	operation = get_op_func(argv[2]);
+
+	
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	
+	printf("%d\n", operation(num1, num2));
+
+	return (0);
+}
+
