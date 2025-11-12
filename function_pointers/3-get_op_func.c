48a85c931e08b303cc801e19571b993529976922
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-11-12 11:28:54 +0000

    add

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,6 +1,5 @@
 #include "3-calc.h"
-#include <stdlib.h>
-
+#include <stddef.h>
 
 int (*get_op_func(char *s))(int, int)
 {
@@ -16,11 +15,10 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (*(ops[i].op) == *s)
+		if (ops[i].op[0] == s[0] && s[1] == '\0')
 			return (ops[i].f);
 		i++;
 	}
-
 	return (NULL);
 }
 
