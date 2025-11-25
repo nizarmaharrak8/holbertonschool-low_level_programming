40c0af31e0a3a6c8583423157767bf9d375b80c6
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-11-25 11:05:12 +0000

    fix

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -5,7 +5,7 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
+ * @str: string
  * @len: length of the string
  * @next: points to the next node
  *
@@ -19,6 +19,7 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif /* LISTS_H */
 
