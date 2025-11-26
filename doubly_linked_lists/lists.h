3aa6622af567f4eba28a768d5a0681aebc7c82a8
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-11-26 11:20:18 +0000

    add

---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+        char *str;
+        unsigned int len;
+        struct list_s *next;
+} list_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif /* LISTS_H */
+
