9fdd114451a5acc04df26f74440e8e6924f89b81
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-12-01 11:56:25 +0000

    fix

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -10,12 +10,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
     ssize_t n_read, n_written;
 
     if (filename == NULL)
-        return (0);
-
+    {
+	    return (0);
+    }
     fd = open(filename, O_RDONLY);
     if (fd == -1)
-        return (0);
-
+    {
+	    return (0);
+    }
     buffer = malloc(letters);
     if (buffer == NULL)
     {
