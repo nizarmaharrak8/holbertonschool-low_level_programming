d89b46ee51d6f99082e0f23fea77bdf22455e494
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-12-08 15:39:29 +0000

    add

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,9 @@
+#include "hash_tables.h"
+#include <string.h>
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+
+if (ht == NULL || key == NULL || key == '\0' || value == NULL)
+    return (0);
+
+
