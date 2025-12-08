1c9c9ea63927f929eefedf44943882c84874161a
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-12-08 11:26:38 +0000

    fix

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,18 +13,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	if (size == 0)
-		return (NULL);
-
+	return (NULL);
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
-		return (NULL);
+	return (NULL);
 
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 	{
 		free(ht);
-		return (NULL);
+	return (NULL);
 	}
 
 	for (i = 0; i < size; i++)
