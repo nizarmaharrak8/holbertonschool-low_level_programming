a8b6c734456d210a576d5de7464659e66f3387b5
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-12-08 13:42:51 +0000

    fix

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,7 +1,6 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -21,7 +20,8 @@ typedef struct hash_node_s
 /**
  * struct hash_table_s - Hash table data structure
  * @size: The size of the array
- * @array: An array of size @size, each cell points to the first node of a linked list
+ * @array: An array of size @size. Each cell points to the first
+ *         node of a linked list.
  */
 typedef struct hash_table_s
 {
@@ -31,7 +31,8 @@ typedef struct hash_table_s
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
+unsigned long int key_index(const unsigned char *key,
+			    unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
