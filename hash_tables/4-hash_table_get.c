4d994d47be4c75b50c7f9b3a69bf58c7f6c98941
root <nizarmaharrak@gmail.com>
2025-12-09 16:12:22 +0000

    fix

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -14,15 +14,15 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
     hash_node_t *node;
 
     if (ht == NULL || key == NULL || *key == '\0')
-        return (NULL);
+    return (NULL);
 
     index = key_index((const unsigned char *)key, ht->size);
     node = ht->array[index];
 
     while (node != NULL)
     {
-        if (strcmp(node->key, key) == 0)
-            return (node->value);
+    if (strcmp(node->key, key) == 0)
+    return (node->value);
 
         node = node->next;
     }
