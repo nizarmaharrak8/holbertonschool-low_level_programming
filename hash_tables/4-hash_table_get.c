a41601d1ac11e3b9cfe61dbb38cf07960e9fff98
root <nizarmaharrak@gmail.com>
2025-12-09 14:39:29 +0000

    add

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -7,5 +7,5 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
                 return (0);
 
-	key_index(key)
+	index= key_index(key)
 
