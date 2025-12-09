bf85225ee77ad75b25b8988efbaad51f6d18f60d
root <nizarmaharrak@gmail.com>
2025-12-09 13:40:08 +0000

    add

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,8 +1,11 @@
 #include "hash_tables_h"
 #include <string>
 
+int index; 
 char *hash_table_get(const hash_table_t *ht, const char *key);
 
 if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
                 return (0);
+
+	key_index(key)
 
